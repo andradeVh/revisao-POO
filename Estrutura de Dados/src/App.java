b5395cc424c3fd5421a096ed1294ecf061b9cb30
andradeVh <andrade.vh18@gmail.com>
2026-07-30 03:35:46 -0300

    Finalizando exercicio 1

--- Estrutura de Dados/src/App.java
+++ Estrutura de Dados/src/App.java
@@ -1,14 +1,20 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
-       Gerente gerente = new Gerente(1, 5000, "João", 1000);
-        Projeto projeto = new Projeto(gerente, "Projeto A");
+        Projeto projeto = new Projeto(new Gerente(1, 5000, "João"), "Projeto X", new ArrayList<Funcionario>());
+
         Funcionario funcionario1 = new Funcionario(2, 3000, "Maria");
         Funcionario funcionario2 = new Funcionario(3, 4000, "Pedro");
+        Funcionario funcionario3 = new Funcionario(4, 3500, "Ana");
 
         projeto.adicionarFuncionario(funcionario1);
         projeto.adicionarFuncionario(funcionario2);
+        projeto.adicionarFuncionario(funcionario3);
 
+        projeto.finalizarProjeto();
 
        System.out.println(projeto.toString());
+    
     }
 }
--- Estrutura de Dados/src/Projeto.java
+++ Estrutura de Dados/src/Projeto.java
@@ -1,74 +1,87 @@
 import java.util.ArrayList;
 
-public class Projeto{
+public class Projeto {
     private Gerente gerente;
     private String nomeProjeto;
     private ArrayList<Funcionario> funcionarios;
     private double custoTotal = 0;
     private boolean projetoFinalizado = false;
 
-    public Projeto(Gerente gerente, String nomeProjeto){
+    public Projeto(Gerente gerente, String nomeProjeto, ArrayList<Funcionario> funcionarios) {
         this.gerente = gerente;
+        this.nomeProjeto = nomeProjeto;
+        this.funcionarios = funcionarios;
+    }
+
+    public void setGerente(Gerente gerente) {
+        this.gerente = gerente;
+    }
+
+    public void setNomeProjeto(String nomeProjeto) {
         this.nomeProjeto = nomeProjeto;
     }
 
-    public void setGerente(Gerente gerente){
-        this.gerente = gerente;
-    }
-
-    public void setNomeProjeto(String nomeProjeto){
-        this.nomeProjeto = nomeProjeto;
-    }
-
-    public Gerente getGerente(){
+    public Gerente getGerente() {
         return this.gerente;
     }
 
-    public String getNomeProjeto(){
+    public String getNomeProjeto() {
         return this.nomeProjeto;
     }
 
-    public double calcularCustoTotal(){
-        custoTotal = gerente.getSalario();
-        for(Funcionario funcionario : funcionarios){
-            custoTotal += funcionario.getSalario();
+    public double calcularCustoTotal() {
+        calcularBonusGerente();
+        custoTotal = gerente.getSalarioFinal();
+
+        for (Funcionario funcionario : funcionarios) {
+            if (projetoFinalizado) {
+                funcionario.setBonus(funcionario.getSalario() * 0.1);
+            }
+            custoTotal += funcionario.getSalarioFinal();
         }
-        custoTotal += gerente.getSalario(); 
         return custoTotal;
     }
 
-    public int qtdFuncionarios(){
+    public int qtdFuncionarios() {
         return funcionarios.size();
     }
 
-    public boolean finalizarProjeto(){
+    public boolean finalizarProjeto() {
         this.projetoFinalizado = true;
         return this.projetoFinalizado;
     }
 
-    public double calcularBonusGerente(){
-        if (projetoFinalizado){
-            return gerente.getSalario() * 0.1 * (qtdFuncionarios() + 1);
+    public double calcularBonusGerente() {
+        double salario = gerente.getSalario();
+        double bonus;
+
+        if (projetoFinalizado) {
+            bonus = salario * 0.1 * (qtdFuncionarios() + 1);
         } else {
-            return gerente.getSalario() * 0.1 * qtdFuncionarios();
+            bonus = salario * 0.1 * qtdFuncionarios();
         }
+
+        this.gerente.setBonus(bonus);
+        return bonus;
     }
 
-    public void adicionarFuncionario(Funcionario funcionario){
+    public void adicionarFuncionario(Funcionario funcionario) {
         funcionarios.add(funcionario);
     }
 
-    public String toString(){
+    public String toString() {
+        calcularBonusGerente();
+        calcularCustoTotal();
+
         String info = "Projeto: " + nomeProjeto + "\n";
         info += "Gerente: " + gerente.getNome() + "\n";
+        info += "Salario do gerente: " + gerente.getSalarioFinal() + "\n";
         info += "Funcionarios: \n";
-        for(Funcionario funcionario : funcionarios){
+        for (Funcionario funcionario : funcionarios) {
             info += funcionario.getNome() + "\n";
-            info += "Salario: " + funcionario.getSalario() + "\n";
+            info += "Salario: " + funcionario.getSalarioFinal() + "\n";
         }
         info += "Custo Total: " + calcularCustoTotal() + "\n";
         return info;
     }
-
-
 }
